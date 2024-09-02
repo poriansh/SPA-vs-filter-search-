744af48b26e919d2a3804019cdedfba4af0fad76
poria neyshaboriyan <poria.nsh@gmail.com>
2024-09-02 21:46:26 +0330

    last committ

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 import Dashboard from "./pages/Dashboard.js";
 import Posts from "./pages/Posts.js";
 import Products from "./pages/Products.js";
-import NotFound from "./pages/NotFound.js";
 const contentMain = document.querySelector("#app");
 const navLinks = document.querySelectorAll(".nav__link");
 const nav = document.querySelector(".nav");
@@ -12,21 +11,9 @@ function router(params) {
     {path: "/posts", view: Posts},
     {path: "/products", view: Products},
   ];
-   const match = routes.find((item) => location.pathname === item.path);
-
-   if (!match) {
-     contentMain.innerHTML = NotFound();
-   } else {
-     contentMain.innerHTML = match.view();
-   }
-
-   // بعد از اضافه کردن محتوای جدید به DOM، اسکریپت‌های مرتبط با آن را اجرا می‌کنیم
-   setTimeout(() => {
-     if (location.pathname === "/products") {
-       const Products = match.view; // استفاده از صفحه Products
-       Products(); // فراخوانی و اجرای توابع مرتبط با این صفحه
-     }
-   }, 0);
+  const match = routes.find((item) => location.pathname === item.path);
+  // اگر مسیر تطابق داشت، نمایش صفحه مربوطه
+  contentMain.innerHTML = match.view();
 }
 
 function navigateTo(url) {
@@ -34,13 +21,15 @@ function navigateTo(url) {
   router();
 }
 toggelerSidebar.addEventListener("click", () => {
-  nav.classList.toggle("nav-active");
+  nav.classList.toggle("nav--active");
 });
 document.addEventListener("DOMContentLoaded", () => {
   router();
   navLinks.forEach((item) => {
     item.addEventListener("click", (e) => {
       e.preventDefault();
+      document.querySelector(".nav__link--active").classList.remove("nav__link--active");
+      item.classList.add("nav__link--active");
       navigateTo(e.target.href);
     });
   });
@@ -50,9 +39,9 @@ window.addEventListener("popstate", router);
 
 function updateNavClass() {
   if (window.matchMedia("(max-width: 768px)").matches) {
-    nav.classList.add("nav-active");
+    nav.classList.add("nav--active");
   } else {
-    nav.classList.remove("nav-active");
+    nav.classList.remove("nav--active");
   }
 }
 // هر بار که اندازه صفحه تغییر می‌کند
--- pages/NotFound.js
+++
@@ -1,9 +0,0 @@
-const NotFound = () => {
-  return `
-    <div class="app-header"></div>
-    <h1>Not-found page</h1>
-    <a href="/" data-link>go to home page?</a>
-    `;
-};
-
-export default NotFound;
